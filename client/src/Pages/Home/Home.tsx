49e94a1a19d4b07d7951ff94cfa805e74d206a69
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-10-28 08:48:00 +0100

    Refacroty(Home.tsx, Home.scss): Rewrite Home TSX and SCSS

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -4,12 +4,12 @@ import "./scss/Home.scss";
 const Home = () => {
   return (
     <main>
-      <h1>
+      <h1 className="text-[2rem] md:text-[2.5rem] lg:text-[3rem]">
         Gain full control
         <br /> over your process
       </h1>
-      <div className="light first"></div>
-      <div className="light secound"></div>
+      <div className="light up"></div>
+      <div className="light down"></div>
     </main>
   );
 };
